d896df76cea74a420142dc4dc6c451ab03d3943f
Brandon Y Lam <me@brandonlam.ca>
2020-12-16 13:48:38 -0800

    refactor question index and show to class

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const QuestionIndexPage = (props) => {
-  return(
-    <main>
-      <h1>Questions</h1>
-      <ul style={{ padding: 0, listStyle: 'none'}}>
-        {
-          props.questions.map(question => {
-            return(
-              <li key={question.id}>
-                <a href='#'>{question.title}</a>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </main>
-  )
+class QuestionIndexPage extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Questions</h1>
+        <ul style={{ padding: 0, listStyle: 'none'}}>
+          {
+            this.props.questions.map(question => {
+              return(
+                <li key={question.id}>
+                  <a href='#'>{question.title}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
+    );
+  }
 }
 
 export default QuestionIndexPage;
--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionDetails from './QuestionDetails';
 import AnswerList from './AnswerList';
 
-const QuestionShowPage = ({ question }) => { // react component
-  // returns react elements
-  // provide props to a element by adding props like you would html element's attributes
-  return(
-    <main>
-      <QuestionDetails
-        title={ question.title }
-        body={ question.body }
-        author={ question.author }
-        view_count={ question.view_count }
-        created_at={new Date(question.created_at)}
-        updated_at={new Date(question.updated_at)}
-      />
-      <h2>Answers:</h2>
-      {/* { AnswerDetails({body: 'blue', author: { full_name: 'bugs bunny'}, created_at: new Date()})} */}
-      {/* Because AnswerDetails is just a function that returns a React Element you can render it out with the above syntax as well but this would be considered bad practice. */}
-      <AnswerList answers={question.answers} />
-    </main>
-  )
+class QuestionShowPage extends Component {
+  // `this` refers to an instance of QuestionShowPage
+  // to access props given to QuestionShowPage we use `this.props`
+  render() {
+    return (
+      <main>
+        <QuestionDetails
+          title={ this.props.question.title }
+          body={ this.props.question.body }
+          author={ this.props.question.author }
+          view_count={ this.props.question.view_count }
+          created_at={new Date(this.props.question.created_at)}
+          updated_at={new Date(this.props.question.updated_at)}
+        />
+        <h2>Answers:</h2>
+        {/* { AnswerDetails({body: 'blue', author: { full_name: 'bugs bunny'}, created_at: new Date()})} */}
+        {/* Because AnswerDetails is just a function that returns a React Element you can render it out with the above syntax as well but this would be considered bad practice. */}
+        <AnswerList answers={this.props.question.answers} />
+      </main>
+    )
+  }
 }
 
 export default QuestionShowPage;
