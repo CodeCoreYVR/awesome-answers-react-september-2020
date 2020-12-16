1c2fcc85a07d6c4c27ec4c6a49a8e33fdab67a29
Brandon Y Lam <me@brandonlam.ca>
2020-12-16 14:29:24 -0800

    add demo for removing question from index

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -11,6 +11,20 @@ class QuestionIndexPage extends Component {
     }
   }
 
+  deleteQuestion(id) {
+    // to change you can only call this.setState
+
+    // .setState accepts a callback, with (state, props) as the arguments. State in the arguments is the current state of the component
+    
+    this.setState((state) => {
+      // setState must always return an object.
+      // this object gets combined with the current state.
+      return {
+        questions: state.questions.filter(q => q.id !== id)
+      }
+    })
+  }
+
   render() {
     return (
       <main>
@@ -21,6 +35,11 @@ class QuestionIndexPage extends Component {
               return(
                 <li key={question.id}>
                   <a href='#'>{question.title}</a>
+                  <button onClick={
+                    () => this.deleteQuestion(question.id)
+                  }>
+                    Delete
+                  </button>
                 </li>
               )
             })
