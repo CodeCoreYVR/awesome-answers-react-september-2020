76ac1294519eab4bda1ccbc54d941da5915e6949
Brandon Y Lam <me@brandonlam.ca>
2020-12-17 14:00:37 -0800

    add hacky sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import QuestionShowPage from './components/QuestionShowPage';
 import QuestionIndexPage from './components/QuestionIndexPage';
 import CurrentDateTime from './components/CurrentDateTime';
+import { Session } from './requests';
 
 import './styles/App.css';
 
@@ -9,8 +10,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clocksCount: [1]
+      clocksCount: [1],
+      user: null
     }
+  }
+
+  componentDidMount() {
+    Session.create({
+      email: 'js@winterfell.gov',
+      password: 'supersecret'
+    })
+    .then(user => {
+      this.setState((state) => {
+        return {
+          user: user
+        }
+      })
+    })
   }
 
   render() {
