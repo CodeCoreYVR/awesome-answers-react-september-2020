aa78aac18322702f77c8871eb02c598a14f0776f
Brandon Y Lam <me@brandonlam.ca>
2020-12-17 13:38:55 -0800

    add CurrentDateTime demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionShowPage from './components/QuestionShowPage';
 import QuestionIndexPage from './components/QuestionIndexPage';
+import CurrentDateTime from './components/CurrentDateTime';
 
 import './styles/App.css';
 
-const App = () => {
-  return(
-    <div>
-      <QuestionIndexPage/>
-      <QuestionShowPage/>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clocksCount: [1]
+    }
+  }
 
-export const B = () => {
-  return(
-    <div>B</div>
-  )
+  render() {
+    return (
+      <div>
+        {/* <QuestionIndexPage/>
+        <QuestionShowPage/> */}
+        {
+          this.state.clocksCount.map((c, i) => {
+            return <CurrentDateTime key={i} shouldShowTime={true}/>
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
